66bfebb7d12c9fafc69f1fa07447471c3b28bf6f
wiki <xieshiyao@1218.com.cn>
2020-03-08 17:37:53 +0800

    提交修改

--- spring_boot_demo/spingboot_web/src/main/java/com/rzx/test/pool/TestPool.java
+++ spring_boot_demo/spingboot_web/src/main/java/com/rzx/test/pool/TestPool.java
@@ -3,5 +3,6 @@ package com.rzx.test.pool;
 public class TestPool {
     public static void main(String[] args) {
         ConnectionPoolManager instance = ConnectionPoolManager.getInstance();
+        System.out.println("123456");
     }
 }
